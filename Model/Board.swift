ce5213d77f71cc2d52c31b96774cf87d237b93e1
Evgeniy Ryshkov <eryshkov@gmail.com>
2019-01-08 12:26:14 +0300

    Board is just added

---
+++ Model/Board.swift
@@ -0,0 +1,13 @@
+//
+//  Board.swift
+//  Four in a Row
+//
+//  Created by Evgeniy Ryshkov on 08/01/2019.
+//  Copyright © 2019 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class Board: NSObject {
+
+}
