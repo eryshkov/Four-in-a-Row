fb1ef0c41e88a0a8816d2d1b873b65ca619702d0
Evgeniy Ryshkov <eryshkov@gmail.com>
2019-01-08 11:57:24 +0300

    Initial Commit

---
+++ Four in a Row/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Four in a Row
+//
+//  Created by Evgeniy Ryshkov on 08/01/2019.
+//  Copyright © 2019 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
